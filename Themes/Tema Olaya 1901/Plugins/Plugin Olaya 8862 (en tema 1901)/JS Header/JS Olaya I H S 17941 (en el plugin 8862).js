565a73b2049da6bf6549f7f7558c3f0f205b61ae
oalonso-protecmedia <88494124+oalonso-protecmedia@users.noreply.github.com>
2021-09-29 14:41:16 +0200

    Add files via upload

---
+++ Themes/Tema Olaya 1901/Plugins/Plugin Olaya 8862 (en tema 1901)/JS Header/JS Olaya I H S 17941 (en el plugin 8862).js
@@ -0,0 +1,10 @@
+﻿/* Prueba de texto de un fichero con referencias a recursos */
+
+var lalala = 0;
+
+// tengo una referencia a la imagen hola.png en una ruta absoluta más larga que la del recurso 
+var zTest1 = "d:\ficheros\test\mis imágenes\hola.png#cosa";
+
+// y otra referencia al hola.xml en una ruta relativa más corta que la del recurso
+var zTest2 = '\hola.xml';
+
